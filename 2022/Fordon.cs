9a0711f9af11267b876e71bd6dc96c6c5d484ac9
Goffer69 <91138373+Goffer69@users.noreply.github.com>
2022-01-11 16:07:20 +0100

    Add files via upload

---
+++ 2022/Fordon.cs
@@ -0,0 +1,11 @@
+class Fordon{
+    private string märke; 
+    public string Märke{
+        get{
+            return märke; 
+        }
+        set{
+            märke = value; 
+        }
+    }
+}
---
+++ 2022/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fordon bil1 = new Fordon();
+            bil1.Märke = "BMW";
+            Console.WriteLine(bil1.Märke);
+        }
+    }
+}
