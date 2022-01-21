863756441cd092cf988549116242a78856932461
Goffer69 <91138373+Goffer69@users.noreply.github.com>
2022-01-21 15:49:07 +0100

    Add files via upload

--- Fordon.cs
+++ Fordon.cs
@@ -1,4 +1,12 @@
 class Fordon{
+
+    public Fordon(string Nyttmärke) {
+        Märke = Nyttmärke;
+    }
+
+    public Fordon(){
+        
+    }
     private string märke; 
     public string Märke{
         get{
--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ namespace _2022
             Fordon bil1 = new Fordon();
             bil1.Märke = "BMW";
             Console.WriteLine(bil1.Märke);
+
+            Fordon bil2 = new Fordon("Ford");
+            Console.WriteLine(bil2.Märke);
         }
+
     }
 }
